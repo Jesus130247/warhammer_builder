5913b72ae5f1aace28c51bdbe481d7729acd0f1c
Jesus130247 <leonardo.monty@hotmail.com>
2024-09-17 16:10:09 +1000

    forgot to drag dist folder

--- warhammer-react-builder/src/components/footer/footer.jsx
+++ warhammer-react-builder/src/components/footer/footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
             <br />
             <span>Prepare for Battle</span> &copy; 2024: Powered by <a target='__blank' href='https://wahapedia.ru/'>Wahapedia</a>
             <br />
-            Created By Leonardo Manteit : last Updated 28/08/2024
+            Created By Leonardo Manteit : last Updated 17/09/2024
         </footer>
     )
 }
